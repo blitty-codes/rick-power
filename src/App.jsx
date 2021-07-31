a96a52b59e5b436df17bbad8c872967e18f0cfc1
blitty-codes <blittynet@gmail.com>
2021-07-31 20:55:29 +0200

    installed react-router and used it

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import Header from './components/header';
-import CardContainer from './components/cardContainer';
-import Search from './components/search';
+import Home from './routes/home';
+import Error from './routes/error';
 
-// this is just an example
-const ricks = [
-	{
-		id: 1,
-		name: 'RickRick Sanchez',
-		gender: 'Male',
-		status: 'Alive',
-		image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-	},
-	{
-		id: 8,
-		name: 'Adjudicator Rick',
-		gender: 'Male',
-		status: 'Dead',
-		image: 'https://rickandmortyapi.com/api/character/avatar/8.jpeg'
-	},
-	{
-		id: 15,
-		name: 'Alien Rick',
-		gender: 'Male',
-		status: 'unknown',
-		image: 'https://rickandmortyapi.com/api/character/avatar/15.jpeg'
-	},
-	{
-		id: 19,
-		name: 'Antenna Rick',
-		gender: 'Male',
-		status: 'unknown',
-		image: 'https://rickandmortyapi.com/api/character/avatar/19.jpeg'
-	},
-	{
-		id: 48,
-		name: 'Black Rick',
-		gender: 'Male',
-		status: 'Alive',
-		image: 'https://rickandmortyapi.com/api/character/avatar/48.jpeg'
-	},
-]
+import './styles/custom.css';
 
 const App = () => (
 	<>
-		<Search input="" />
-		<header>
-			<Header title="Welcome to Ricks deck!!!" />
-		</header>
-		<hr className="mt-6 mb-5 w-2/5 m-auto" />
-		<main>
-			<CardContainer characters={ricks} />
-		</main>
+		<Switch>
+			<Route path="/" component={Home} exact />
+			<Route component={Error} exact />
+		</Switch>
 	</>
 );
 
