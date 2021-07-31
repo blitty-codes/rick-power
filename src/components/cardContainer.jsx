f8a05bf28e16ce67cbe1693a918520ec5fa1bc16
blitty-codes <blittynet@gmail.com>
2021-07-31 20:58:21 +0200

    Added feature to fetch data from a query given

--- src/components/cardContainer.jsx
+++ src/components/cardContainer.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useQuery } from '@apollo/client';
 
 import Card from './card';
 
-const CardContainer = ({ characters }) => (
-	<div className="mb-5 p-2 border-gray-900 border-2 w-max m-auto rounded-md flex flex-col flex-nowrap items-center md:grid md:grid-flow-row md:grid-cols-3 md:grid-rows-1 md:gap-4 lg:p-1 lg:grid-rows-2">
-		{
-			characters.map(({id, name, gender, status, image}) => (
-				<Card
-					key={id}
-					name={name}
-					gender={gender}
-					status={status}
-					image={image}
-				/>
-			))
-		}
-	</div>
-);
+const CardContainer = ({ query }) => {
+	const { loading, error, data } = useQuery(query);
 
-CardContainer.propTypes = ({ characters: PropTypes.array });
+	if (loading) return <h3>Loading...</h3>
+	if (error) return <h3>Error :(</h3>
+
+	let characters = data.characters.results;
+	const init = Math.random() * ((characters.length-6) - 0) + 0;
+	characters = characters.slice(init, init+5);
+	return (
+		<div className="mb-5 p-2 w-max m-auto flex flex-col flex-nowrap items-center md:grid md:grid-flow-row md:grid-cols-3 md:grid-rows-1 md:gap-4 lg:p-1 lg:grid-rows-2">
+			{
+				characters.map(({id, name, gender, status, image}) => (
+					<Card
+						key={id}
+						name={name}
+						gender={gender}
+						status={status}
+						image={image}
+					/>
+				))
+			}
+		</div>
+	);
+};
+
+CardContainer.propTypes = ({ query: PropTypes.string });
 
 export default CardContainer;
