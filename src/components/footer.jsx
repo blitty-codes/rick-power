1d2d6c9f585385e6bb8c84f19927c0596e3bad4f
blitty-codes <blittynet@gmail.com>
2021-08-09 18:22:24 +0200

    Basic footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<div className="footer mt-4">
+			<a className="text-xl" href="https://github.com/blitty-codes">blitty-codes</a>
+		</div>
+	);
+};
+
+export default Footer;
