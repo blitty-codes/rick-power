5d8350c6ce0ea239894f620a1e5cd39ee56807d5
blitty-codes <blittynet@gmail.com>
2021-08-09 18:20:22 +0200

    Styled erros

---
+++ src/components/customError.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomError = () => (
+	<h1
+		className="mt-96 mb-20 text-8xl text-center text-red-600 font-extrabold pb-4"
+	>
+		Error!!!
+	</h1>
+);
+
+export default CustomError;
