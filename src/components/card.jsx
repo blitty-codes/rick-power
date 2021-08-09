f2bdee8d41214eff7f13fdafb3be1c514226790a
blitty-codes <blittynet@gmail.com>
2021-08-09 18:18:26 +0200

    functionaily of the button

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Card = ({ name, gender, status, image }) => {
+const Card = ({ id, name, gender, status, image }) => {  
   return (
     <div
 			className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 mb-1 lg:py-4 lg:space-y-0 lg:space-x-6 lg:m-1.5">
@@ -16,15 +17,20 @@ const Card = ({ name, gender, status, image }) => {
           <p className="text-gray-500 font-medium">{ gender }</p>
 					<p className="text-gray-500 text-opacity-75 font-medium">{status === 'Alive' ? '🟢' : status === 'unknown' ? '❓' : '🔴'} { status }</p>
         </div>
-        <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-          More information
-        </button>
+        <Link to={`/charInfo/${id}`}>
+          <button
+            className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+          >
+            More information
+          </button>
+        </Link>
       </div>
     </div>
   );
 };
 
 Card.propTypes = {
+  id: PropTypes.any,
   name: PropTypes.string,
 	gender: PropTypes.string,
 	status: PropTypes.string,
